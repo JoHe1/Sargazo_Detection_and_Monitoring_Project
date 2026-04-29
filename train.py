6b5fd8302a0b9f8df1007560cc2a718f821554a0
Jorge <jorgelorenzo188@gmail.com>
2026-04-30 00:09:46 +0100

    train update to solve nan problems

--- train.py
+++ train.py
@@ -141,6 +141,7 @@ def train(config: ExperimentConfig) -> None:
             outputs = model(images)
             loss    = criterion(outputs, masks)
             loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
             train_loss += loss.item()
 
